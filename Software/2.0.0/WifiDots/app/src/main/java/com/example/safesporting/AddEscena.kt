8dd2ed569fc871a2bcd1fade64564988e16ccfe2
= <lucasdavalle25@gmail.com>
2023-05-18 10:40:16 -0300

    Se agrega version 3.0 de app

---
+++ Software/2.0.0/WifiDots/app/src/main/java/com/example/safesporting/AddEscena.kt
@@ -0,0 +1,11 @@
+package com.example.safesporting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Escena : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_escena)
+    }
+}
